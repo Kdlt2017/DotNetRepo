512db1fe913bfac296369a6ba18cb69573f9b0aa
Kdlt2017 <toussaint.kebou@yahoo.com>
2019-04-26 15:39:49 +0200

    Latest updates

---
+++ MyFinance/MyFinance.Presentation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyFinance.Presentation.MvcApplication" Language="C#" %>
---
+++ MyFinance/MyFinance.Presentation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyFinance.Presentation.Startup))]
+namespace MyFinance.Presentation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
