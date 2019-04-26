ab5ee7f15ec70978a6fd74b7375967c2ee8559cf
Kdlt2017 <toussaint.kebou@yahoo.com>
2019-04-26 16:14:09 +0200

    Latest changes

---
+++ MyFinance/MyFinance.Presentation/Controllers/ProductController.cs
@@ -0,0 +1,106 @@
+﻿using MyFinance.Domain.Entities;
+using MyFinance.Presentation.Models;
+using MyFinance.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFinance.Presentation.Controllers
+{
+    public class ProductController : Controller
+    {
+        //Calling on the service to consume the prepared
+        //functionalities
+        IProductService MyService;
+        public ProductController()
+        {
+            MyService = new ProductService();
+        }
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(ProductVM pr)
+        {
+            //Instantiated a product to convert the
+            //ProductVM(Product View Model) into a product
+            //So as to add the product into the database
+            /*Known as an explicit conversion*/
+            Product prod = new Product() {
+                DateProd = pr.DateProd,
+                Description = pr.Description,
+                Name = pr.Name,
+                Price = pr.Price,
+                Quantity = pr.Quantity,
+                ImageUrl2 = pr.ImageUrl2
+            };
+            MyService.AddProduct(prod);
+            
+           
+                return View();
+            
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MyFinance/MyFinance.Presentation/Models/ProductVM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyFinance.Presentation.Models
+{
+    public class ProductVM
+    {
+
+        [Display(Name = "Production Date")]
+        [DataType(DataType.Date)]
+        public DateTime DateProd { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "This field is required")]
+        [StringLength(25, ErrorMessage = "User Input should be 25 characters")]
+        [MaxLength(50, ErrorMessage = "Max length 50 characters")]
+        public string Name { get; set; }
+        [DataType(DataType.Currency)]
+        public double Price { get; set; }
+        public int ProductId { get; set; }
+        [Range(0, int.MaxValue)]
+        public int Quantity { get; set; }
+        public string ImageUrl2 { get; set; }
+
+        
+    }
+}
